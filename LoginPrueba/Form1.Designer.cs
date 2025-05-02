e194da7fa0b746773e91fcea4f3966964325f79d
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2025-05-01 21:27:21 -0300

    Agregado de capas de login, negocio y datos

---
+++ LoginPrueba/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace LoginPrueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCancelar = new System.Windows.Forms.Button();
+            this.BtnIniciarSesion = new System.Windows.Forms.Button();
+            this.LblUSuario = new System.Windows.Forms.Label();
+            this.LblContrasena = new System.Windows.Forms.Label();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.textContrasena = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCancelar
+            // 
+            this.BtnCancelar.Location = new System.Drawing.Point(384, 223);
+            this.BtnCancelar.Name = "BtnCancelar";
+            this.BtnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancelar.TabIndex = 0;
+            this.BtnCancelar.Text = "Cancelar";
+            this.BtnCancelar.UseVisualStyleBackColor = true;
+            this.BtnCancelar.Click += new System.EventHandler(this.BtnCancelar_Click);
+            // 
+            // BtnIniciarSesion
+            // 
+            this.BtnIniciarSesion.Location = new System.Drawing.Point(221, 223);
+            this.BtnIniciarSesion.Name = "BtnIniciarSesion";
+            this.BtnIniciarSesion.Size = new System.Drawing.Size(75, 23);
+            this.BtnIniciarSesion.TabIndex = 1;
+            this.BtnIniciarSesion.Text = "Iniciar Sesión";
+            this.BtnIniciarSesion.UseVisualStyleBackColor = true;
+            // 
+            // LblUSuario
+            // 
+            this.LblUSuario.AutoSize = true;
+            this.LblUSuario.Location = new System.Drawing.Point(218, 95);
+            this.LblUSuario.Name = "LblUSuario";
+            this.LblUSuario.Size = new System.Drawing.Size(43, 13);
+            this.LblUSuario.TabIndex = 2;
+            this.LblUSuario.Text = "Usuario";
+            // 
+            // LblContrasena
+            // 
+            this.LblContrasena.AutoSize = true;
+            this.LblContrasena.Location = new System.Drawing.Point(218, 154);
+            this.LblContrasena.Name = "LblContrasena";
+            this.LblContrasena.Size = new System.Drawing.Size(61, 13);
+            this.LblContrasena.TabIndex = 3;
+            this.LblContrasena.Text = "Contraseña";
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Location = new System.Drawing.Point(322, 95);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(167, 20);
+            this.textBoxUsuario.TabIndex = 4;
+            // 
+            // textContrasena
+            // 
+            this.textContrasena.Location = new System.Drawing.Point(322, 147);
+            this.textContrasena.Name = "textContrasena";
+            this.textContrasena.PasswordChar = '*';
+            this.textContrasena.Size = new System.Drawing.Size(167, 20);
+            this.textContrasena.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 449);
+            this.Controls.Add(this.textContrasena);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Controls.Add(this.LblContrasena);
+            this.Controls.Add(this.LblUSuario);
+            this.Controls.Add(this.BtnIniciarSesion);
+            this.Controls.Add(this.BtnCancelar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCancelar;
+        private System.Windows.Forms.Button BtnIniciarSesion;
+        private System.Windows.Forms.Label LblUSuario;
+        private System.Windows.Forms.Label LblContrasena;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.TextBox textContrasena;
+    }
+}
+
